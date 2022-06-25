26746872ec5f56129e430e97bd629b3dad4f13d2
gsdkira <larreakelly@gmail.com>
2022-06-24 23:31:29 -0400

    creating typescript todo

---
+++ src/components/InputField.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./styles.css"
+
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+const InputField = ({ todo, setTodo }: Props) => {
+  return (
+    <form className ='input'>
+    <input type="input"
+    value={todo}
+    onChange={
+        (e)=>setTodo(e.target.value)
+    }
+    placeholder="Enter a Task" className='input_box' />
+    <button className='input_submit' type='submit'>GO</button>
+    </form>
+  )
+}
+
+export default InputField
